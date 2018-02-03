e06e032896ad5c592d83d1c46221c315c0d1d86f
Brian Chen <brianchen1997@gmail.com>
2018-02-03 14:50:25 -0500

    testing

--- index.js
+++ index.js
@@ -5,36 +5,22 @@ import LinearGradient from 'react-native-linear-gradient';
 
 
 export default class AvalonApp extends Component {
-    render() {
-        return (
-            <View style={styles.main_container}>
-                <Text style={styles.title}>Avalon Mobile</Text>
-                
-                <TouchableOpacity style={styles.button}>
-                    <Text>PLAY</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
-                    <Text>MULTI-PHONE</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
-                    <Text>STATISTICS</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
-                    <Text>HELP</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
-                    <Text>SETTINGS</Text>
-                </TouchableOpacity>
+  render() {
+    return (
+      <View style={styles.main_container}>
+        {/* <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']}>
+          <Text style={styles.title}>Avalon Mobile</Text>
 
-                // Within your render function
-                <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
-                <Text style={styles.buttonText}>
-                    Sign in with Facebook
-                </Text>
-                </LinearGradient>
-            </View>
-        );
-    }
+          <TouchableOpacity>
+            <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient_button}>
+              <Text style={styles.buttonText}>
+                Sign in with Facebook
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </LinearGradient> */}
+      </View>
+    );
+  }
 }
-
 AppRegistry.registerComponent('AvalonMobile', () => AvalonApp);
--- style.js
+++ style.js
@@ -1,48 +1,34 @@
 import React, { StyleSheet } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient';
 
 export default StyleSheet.create({
 
-    title: {
-        alignItems: 'center',
-        marginTop: 20,
-        padding: 20,
-        fontSize: 20,
-        fontWeight: 'bold'
-     },
-     main_container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingHorizontal: 10,
-        borderColor: '#d6d7da',
-    },
-    button: {
-        alignItems: 'center',
-        // backgroundColor: 'red',
-        background: linear-gradient(red, yellow),
-        padding: 10,
-        width: 200,
-        marginTop: 10 
-    },
-    linearGradient: {
-        flex: 1,
-        paddingLeft: 15,
-        paddingRight: 15,
-        borderRadius: 5
-      },
-      buttonText: {
-        fontSize: 18,
-        fontFamily: 'Gill Sans',
-        textAlign: 'center',
-        margin: 10,
-        color: '#ffffff',
-        backgroundColor: 'transparent',
-      },
-    play_button: {
-        alignItems: 'center',
-        backgroundColor: 'green',
-        padding: 10,
-        width: 200,
-        marginTop: 10 
-    }
+  title: {
+    alignItems: 'center',
+    marginTop: 20,
+    padding: 20,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  main_container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d6d7da',
+    resizeMode: 'cover'
+  },
+  linearGradient_button: {
+    height: 40,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+    marginBottom: 20
+  },
+  buttonText: {
+    fontSize: 18,
+    textAlign: 'center',
+    margin: 7,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  }
 });
