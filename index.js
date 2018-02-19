ce65ebb9b6670bce862509f28a1c48fb8074f6cd
Brian Chen <brianchen1997@gmail.com>
2018-02-19 11:15:52 -0500

    Merged new file structure from master

--- index.js
+++ index.js
@@ -2,22 +2,4 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './src';
 
-<<<<<<< HEAD
-import HomeScreen from "./src/views/HomeScreen"
-import SetupScreen from "./src/views/SetupScreen"
-
-const RootNavigator = StackNavigator({
-    Home: {
-        screen: HomeScreen,
-    },
-    Setup: {
-        screen: SetupScreen
-    }
-});
-
-export default RootNavigator;
-
-AppRegistry.registerComponent('AvalonMobile', () => RootNavigator);
-=======
 AppRegistry.registerComponent('AvalonMobile', () => App);
->>>>>>> master
--- src/views/HomeScreen/HomeScreen.js
+++ src/views/HomeScreen/HomeScreen.js
@@ -1,29 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { StackNavigator } from 'react-navigation';
-import LinearGradient from 'react-native-linear-gradient';
-import { AppRegistry, Text, View, Button, TouchableOpacity } from 'react-native';
-import styles from './style'
-
-export default class HomeScreen extends Component {
-  render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <View style={styles.main_container}>
-        <Text style={styles.title}>Avalon Mobile</Text>
-        <TouchableOpacity style={styles.button} onPress={() => navigate('Setup', { gameObject: {name: 'test', numRounds: 3} })}>
-            <Text>Play</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-          <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
-            <Text style={styles.buttonText}>
-              Sign in with Facebook
-            </Text>
-          </LinearGradient>
-        </TouchableOpacity>
-      </View>
-=======
 import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './HomeScreenStyle'
 
@@ -43,7 +18,6 @@ export default class HomeScreen extends Component {
             <Text>Play</Text>
           </TouchableOpacity>
         </View>
->>>>>>> master
     );
   }
 }
