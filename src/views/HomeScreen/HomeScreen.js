2580e8f20fd00d33842287a6e7ffd767c4d2b63b
Brian Chen <brianchen1997@gmail.com>
2018-02-24 15:05:27 -0500

    Finished basic button component, added icons (#12)

--- src/views/HomeScreen/HomeScreen.js
+++ src/views/HomeScreen/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './HomeScreenStyle'
-
+import SelectButton from '../../components/SelectButton/SelectButton'
 import Background from "../../components/Background/Background";
 
 export default class HomeScreen extends Component {
@@ -17,6 +17,9 @@ export default class HomeScreen extends Component {
           <TouchableOpacity style={styles.button} onPress={() => this.props.navigate(navigateParams)}>
             <Text>Play</Text>
           </TouchableOpacity>
+          <SelectButton onPress={() => console.log('press')}>
+            Play2!
+          </SelectButton>
         </Background>
     );
   }
