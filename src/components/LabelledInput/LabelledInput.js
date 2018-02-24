b446f4244c40a404aeb24a4460ce6df416555704
Daniel Ku <danielku97@gmail.com>
2018-02-24 16:13:28 -0500

    Text input with PNG image

---
+++ src/components/LabelledInput/LabelledInput.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View, TextInput, Image} from 'react-native';
+import styles from './LabelledInputStyle'
+
+
+/*
+HOW TO IMPLEMENT:
+Example:
+ <LabelledInput value={this.state.name} placeholder="Only appears if no value"
+   icon={require('../../img/test_icon .png')} onChangeText={ (text) => this.changeTextInputValue(text) }/>
+
+Also required to implement the onChangeText function where you call LabelledInput
+
+  changeTextInputValue (text) {
+    this.setState({
+      name: text
+    });
+  }
+
+*/
+export default class LabelledInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.sectionStyle}>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.props.onChangeText(text) }
+            value={this.props.value}
+            placeholder={this.props.placeholder ? this.props.placeholder : ''}
+          />
+          <Image source={this.props.icon} style={styles.imageStyle} />
+        </View>
+      </View>
+    );
+  }
+}
---
+++ src/components/LabelledInput/LabelledInputStyle.js
@@ -0,0 +1,39 @@
+import React, { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+
+  input: {
+    fontFamily: "Roboto-Medium",
+    fontSize: 15,
+    flex: 1,
+    backgroundColor: '#fff',
+    color: '#424242',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10
+  },
+
+  sectionStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderWidth: .5,
+    borderColor: '#000',
+    height: 40,
+    borderRadius: 5 ,
+    margin: 10
+  },
+
+  imageStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode : 'stretch',
+    alignItems: 'center'
+  },
+});
