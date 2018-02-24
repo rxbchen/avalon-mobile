744e21151a81a044a753cac80f9c6f1b442f7b4e
Brian Chen <brianchen1997@gmail.com>
2018-02-24 14:22:55 -0500

    Finished basic button component, added icons

--- src/components/SelectButton/SelectButton.js
+++ src/components/SelectButton/SelectButton.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import { Alert, Text, View, TouchableOpacity } from 'react-native'
+import { Alert, Image, Text, View, TouchableOpacity } from 'react-native'
 import styles from './SelectButtonStyle'
 // Button component
-class SelectButton extends Component {
+export default class SelectButton extends Component {
   constructor(props) {
     super(props)
     this.state = { isSelected: false }
   }
-  _onPressButton() {
-    this.setState( { isSelected: !this.state.isSelected })
-    Alert.alert('Hai')
+  // onPressButton() {
+  //   this.setState( { isSelected: !this.state.isSelected })
+  //   Alert.alert(this.state.isSelected)
+  // }
+  
+  onPress = () => {
+    this.setState({
+      isSelected: !this.state.isSelected
+    })
+    Alert.alert(this.state.isSelected.toString())
   }
 
   render() {
     return (
-      <TouchableOpacity onPress={() => this._onPressButton()} style={this.props.isSelected ? styles.selectedButton : styles.unSelectedButton}>
-        <Text style={styles.textStyle}>{this.props.children}</Text>
+      <TouchableOpacity onPress={this.onPress} style={this.props.isSelected ? styles.selectedButton : styles.unSelectedButton}>
+          <View style={ this.props.icon ? styles.sectionStyleNoIcon : styles.sectionStyleIcon}>
+            <Text style={styles.textStyle}>{this.props.children}</Text>
+            <Image source={this.props.icon} style={styles.buttonIcon}/>   
+          </View>     
       </TouchableOpacity>
     );
   }
 }
-
-export default SelectButton;
--- src/components/SelectButton/SelectButtonStyle.js
+++ src/components/SelectButton/SelectButtonStyle.js
@@ -7,23 +7,57 @@ export default StyleSheet.create({
     backgroundColor: '#5d1414',
     borderRadius: 5,
     borderWidth: 1,
-    marginLeft: 5,
-    marginRight: 5
+    margin: 10
   },
   unSelectedButton: {
     alignSelf: 'stretch',
-    backgroundColor: '#5d1414',
+    backgroundColor: '#5d1413',
     borderRadius: 5,
     borderWidth: 1,
-    marginLeft: 5,
-    marginRight: 5
+    margin: 10
   },
   textStyle: {
     alignSelf: 'center',
+    textAlign: 'center',
     color: '#FFF',
     fontSize: 26,
     fontWeight: "100",
     paddingTop: 10,
     paddingBottom: 10
+  },
+  buttonIcon: {
+    height: 25,
+    width: 25,
+    justifyContent: 'center'
+  },
+  sectionStyleNoIcon: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: .5,
+    height: 40,
+    borderRadius: 5 ,
+    margin: 10,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  sectionStyleIcon: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: .5,
+    height: 40,
+    borderRadius: 5 ,
+    margin: 10,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  ImageStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode : 'stretch',
+    alignItems: 'center'
   }
 });
