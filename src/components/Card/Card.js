2a28f3034e3f3c4b7388dc52d47b94c61a0e4143
Quin Millard <midnitefreddy@hotmail.com>
2018-02-24 17:20:56 -0500

    linear gradient worksgit add .git add .

---
+++ src/components/Card/Card.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Text, View, TouchableOpacity, Image } from 'react-native';
+import Collapsible from 'react-native-collapsible'
+import proptypes from 'prop-types'
+import styles from './CardStyle'
+import LinearGradient from 'react-native-linear-gradient';
+
+
+
+export default class Card extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        isCollapsed: this.props.collapsed
+    }
+  }
+
+  toggleCollapse() {
+    this.setState({isCollapsed: !this.state.isCollapsed})
+  }
+
+  render() {
+    return (
+      <View>
+        <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
+          <TouchableOpacity style={styles.titleContainer} onPress={() => this.toggleCollapse()}>
+            <Text style={styles.titleText}>{this.props.title}</Text>
+           <Image style={styles.titleImage} src={this.props.icon} />
+          </TouchableOpacity>
+        </LinearGradient>
+        <Collapsible style={styles.collapsibleContainer} collapsed={this.state.isCollapsed}>
+          {this.props.children}
+        </Collapsible>
+      </View>
+    )
+  }
+}
+
+Card.propTypes = {
+  title: proptypes.string.isRequired,
+  children: proptypes.element.isRequired,
+  collapsed: proptypes.bool,
+  icon: proptypes.any
+}
+
+Card.defaultProps = {
+  collapsed: true,
+  icon: null
+}
---
+++ src/components/Card/CardStyle.js
@@ -0,0 +1,23 @@
+import React, { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  titleContainer: {
+
+  },
+
+  titleText: {
+
+  },
+
+  titleImage: {
+
+  },
+
+  collapsibleContainer: {
+
+  },
+
+  linearGradient: {
+
+  }
+})
