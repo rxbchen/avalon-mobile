95be7fafb5e5ca6083c53ed3c35fd3ebf45b083b
James Kim <jameskim08@users.noreply.github.com>
2018-03-10 15:45:54 -0500

    James/combine setup screen (#24)
    * added night screen
    
    * fixed a dumb bug that react native doesnt like text in view and it wants it in its own view which is stupid
    
    * continue button assigns roles

--- src/views/HomeScreen/HomeScreenStyle.js
+++ src/views/HomeScreen/HomeScreenStyle.js
@@ -1,42 +1,42 @@
 import React, { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-    title: {
-      textAlign: "center",
-      fontFamily: "Roboto-Light",
-      color: '#FFF',
-      fontSize: 60,
-      fontWeight: '100',
-      lineHeight: 55
-    },
-    linearGradient: {
-      height: 40,
-      paddingLeft: 15,
-      paddingRight: 15,
-      borderRadius: 5,
-      marginBottom: 20
-    },
-    buttonText: {
-      fontSize: 18,
-      textAlign: 'center',
-      margin: 7,
-      color: '#ffffff',
-      backgroundColor: 'transparent',
-    },
-    mainButton: {
-      margin: 10,
-      alignSelf: 'center',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: 300,
-      height: 70
-    },
-    smallButton: {
-      margin: 10,
-      alignSelf: 'center',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: 230,
-      height: 60
-    },
-  });
+  title: {
+    textAlign: "center",
+    fontFamily: "Roboto-Light",
+    color: '#FFF',
+    fontSize: 60,
+    fontWeight: '100',
+    lineHeight: 55
+  },
+  linearGradient: {
+    height: 40,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+    marginBottom: 20
+  },
+  buttonText: {
+    fontSize: 18,
+    textAlign: 'center',
+    margin: 7,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
+  mainButton: {
+    margin: 10,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    height: 70
+  },
+  smallButton: {
+    margin: 10,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 230,
+    height: 60
+  },
+})
