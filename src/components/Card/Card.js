a9c823b5e359feefe35d1b9cd69e05706e934f5a
Quin Millard <midnitefreddy@hotmail.com>
2018-06-03 21:22:11 -0400

    removed dumb line

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Text, View, TouchableOpacity, Image } from 'react-native'
-import { Icon } from 'react-native-elements'
 import Collapsible from 'react-native-collapsible'
 import proptypes from 'prop-types'
 import styles from './CardStyle'
